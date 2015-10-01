d62657c6606110ffcdba52dfab4c9f391a545651
shaohua <hshnju@gmail.com>
2015-10-01 15:01:06 +0800

    remove unnecessary things

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var x = 10;
-var y =20;
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
